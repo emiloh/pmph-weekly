ced152d30f6524fcd18e1492f63da5f00a1c2193
emiloh <Emil@houlborg.dk>
2024-09-07 16:06:11 +0200

    Sequential implementation and standard makefile

---
+++ wa1-code-handin/cuda/wa1-task3.cu
@@ -0,0 +1,64 @@
+#include <cuda_runtime.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define GPU_RUNS 300
+#define MAX_N 500000000
+
+__global__ void Kernel(float *x) {
+  // fill me
+}
+
+void run_cpu(unsigned int N, float *in, float *out);
+void run_gpu(unsigned int N, float *h_in, float *h_out, float *d_in,
+             float *d_out);
+void validate(unsigned int N, float *cpu_out, float *gpu_out);
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Invalid number of arguments: %d. Expected 1!\n", argc);
+    exit(1);
+  }
+
+  unsigned int N = atoi(argv[1]);
+  printf("Size of array: %d\n", N);
+
+  if (N > MAX_N) {
+    printf("Size of array too big. Maximal size is %d.\n", MAX_N);
+    exit(1);
+  }
+
+  // Allocating host memory
+  float *h_in;
+  float *h_out_cpu;
+  float *h_out_gpu;
+
+  for (unsigned int i = 0; i < N; ++i) {
+    h_in[i] = (float)i;
+  }
+
+  // Run sequentially
+  run_cpu(N, h_in, h_out_cpu);
+
+  // Allocate device memory
+  float *d_in;
+  float *d_out;
+
+  // Free memory
+  free(h_in);
+  free(h_out_cpu);
+  free(h_out_gpu);
+  free(d_in);
+  free(d_out);
+}
+
+void run_cpu(unsigned int N, float *in, float *out) {
+  // time start
+  for (unsigned int i; i < N; ++i) {
+    out[i] = powf((in[i] / (in[i] - 2.3)), 3.0);
+  }
+  printf("Done, last element: %f\n", out[N - 1]);
+  // time end
+}
