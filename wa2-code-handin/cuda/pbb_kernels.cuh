978ceb9c7e294c64fd38c503c347c1416dfabf57
emiloh <emil@houlborg.dk>
2024-09-14 16:31:17 +0200

    Add solution to task 2

--- wa2-code-handin/cuda/pbb_kernels.cuh
+++ wa2-code-handin/cuda/pbb_kernels.cuh
@@ -436,7 +436,11 @@ copyFromGlb2ShrMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for(uint32_t i=0; i<CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+	#if 0 then
+	    uint32_t loc_ind = i * CHUNK + threadIdx.x;
+	#else
+            uint32_t loc_ind = threadIdx.x*CHUNK + i;
+	#endif
         uint32_t glb_ind = glb_offs + loc_ind;
         T elm = ne;
         if(glb_ind < N) { elm = d_inp[glb_ind]; }
@@ -466,7 +470,11 @@ copyFromShr2GlbMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for (uint32_t i = 0; i < CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x * CHUNK + i;
+	#if 0 then 
+	    uint32_t loc_ind = i * CHUNK + threadIdx.x;
+	#else
+	    uint32_t loc_ind = threadIdx.x * CHUNK + i;
+	#endif
         uint32_t glb_ind = glb_offs + loc_ind;
         if (glb_ind < N) {
             T elm = const_cast<const T&>(shmem_red[loc_ind]);
